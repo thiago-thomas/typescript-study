d6adbb4655ae40f2ede5638f57a55ad260bab1ed
Thiago Thomas <thiago_rocha17@hotmail.com>
2024-09-22 22:54:54 +0100

    Lesson #01 - Variables and Types

---
+++ Lessons/#01 - Variables and Types/App.tsx
@@ -0,0 +1,25 @@
+function App() {
+
+  let testString: string;
+  testString = 'aaa'
+  
+  let testBoolean: boolean;
+  testBoolean = true;
+  
+  let testNumber: number;
+  testNumber = 123;
+
+  let testAny: any;
+  testAny = 'abc123';
+
+  let testArrayString: string[];
+  testArrayString = ['test1', 'test2'];
+
+  return (
+    <>
+      <h1>Hello TS</h1>
+    </>
+  )
+}
+
+export default App
